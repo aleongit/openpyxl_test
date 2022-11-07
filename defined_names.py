85996ac6ee774e9e5db28ea384d1ed5c849986e0
aleongit <aleix.leon@gmail.com>
2022-11-07 11:17:29 +0100

    creating new named ranges

--- defined_names.py
+++ defined_names.py
@@ -1,3 +1,4 @@
+import openpyxl
 from openpyxl import load_workbook
 
 # Defined Names_______________________________________________________________
@@ -52,4 +53,20 @@ for rang in cells:
         print()
 
 # Creating new named ranges_________________________________________
-# TODO:
+
+new_range = openpyxl.workbook.defined_name.DefinedName('newrange', attr_text='Hoja1!$E$1:$H$5')
+wb.defined_names.append(new_range)
+
+# create a local named range (only valid for a specific sheet)
+sheetid = wb.sheetnames.index('Hoja1')
+private_range = openpyxl.workbook.defined_name.DefinedName('privaterange', attr_text='Hoja1!$J$1:$J$5', localSheetId=sheetid)
+wb.defined_names.append(private_range)
+# this local range can't be retrieved from the global defined names
+assert('privaterange' not in wb.defined_names)
+
+# the scope has to be supplied to retrieve local ranges:
+print(wb.defined_names.localnames(sheetid))
+print(wb.defined_names.get('privaterange', sheetid).attr_text)
+
+# save
+wb.save(filename = file)
